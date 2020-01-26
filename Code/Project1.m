acdf959e57649bbde161e173880eb19cebbd0485
Nikos Trembois <ntrembois@gmail.com>
2020-01-26 11:46:10 -0800

    Some minor changes

--- Code/Project1.m
+++ Code/Project1.m
@@ -4,8 +4,6 @@
 
 %% Part 1: Fractals
 phi = @(z) z^2;
-fp1 = 0;
-fp2 = 0;
 a = linspace(-1,1,500);
 b = linspace(-1,1,500);
 M = ones(length(a),length(b));
@@ -24,38 +22,41 @@ for r = 1:length(a)
     end
 end
 
-% Here is a change
-
 colormap([1 0 0; 1 1 1]);
 image( [-1 1], [-1 1], M)
 axis xy
 axis('equal')
 
 %% Part 2:  Fractals
-phi = @(z) z^2 - 1.25;
-fp1 = (1+sqrt(6))/2;
-fp2 = (1-sqrt(6))/2;
-a = linspace(-1.8,1.8,500);
-b = linspace(-.7,.7,500);
-M = ones(length(a),length(b));
-
-for r = 1:length(a)
-    for i = 1:length(b)
-        clear z;
-        z = a(r) + 1i*b(i);
-        for j = 1:100
-            z(j+1) = phi(z(j));
-            if abs(z(j+1)) > 2
-                M(r,i) = 2;
-                break;
+clear M
+phi = @(z,c) z^2 - c;
+a = linspace(-1,1,100);
+b = linspace(-1,1,100);
+c = [0.36 + 0.1i, -.123 - .745i];
+for k = 1:length(c)
+    M{k} = ones(length(a),length(b));
+    for r = 1:length(a)
+        for i = 1:length(b)
+            clear z;
+            z = a(r) + 1i*b(i);
+            for j = 1:100
+                z(j+1) = phi(z(j),c(k));
+                if abs(z(j+1)) > 2
+                    M{k}(r,i) = 2;
+                    break;
+                end
             end
         end
     end
 end
 
-colormap([1 0 0; 1 1 1]);
-image( [-1.8 1.8], [-.7 .7], M)
-axis xy
-axis('equal')
+for i = 1:length(c)
+    figure(); hold on
+    colormap([1 0 0; 1 1 1]);
+    image( [-1 1], [-1 1], M{i})
+    axis xy
+    axis([ -1 1 -1 1])
+    hold off
+end
 
 %% Part 3: Julia Sets
