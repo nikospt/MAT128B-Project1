edf9f7049c1a3c9ac033817a79eb9a3cf784c5aa
Nikos Trembois <ntrembois@gmail.com>
2020-01-26 11:16:13 -0800

    Renamed a file because Matlab complained the name conflicted with a function name

--- Code/Project1.m
+++ Code/Project1.m
@@ -2,7 +2,7 @@
 % UC Davis Winter 2020
 % Nikos Trembois, Caitlin Brown, and
 
-%% Fractals
+%% Part 1: Fractals
 phi = @(z) z^2;
 fp1 = 0;
 fp2 = 0;
@@ -31,7 +31,7 @@ image( [-1 1], [-1 1], M)
 axis xy
 axis('equal')
 
-%% Fractals
+%% Part 2:  Fractals
 phi = @(z) z^2 - 1.25;
 fp1 = (1+sqrt(6))/2;
 fp2 = (1-sqrt(6))/2;
@@ -57,3 +57,5 @@ colormap([1 0 0; 1 1 1]);
 image( [-1.8 1.8], [-.7 .7], M)
 axis xy
 axis('equal')
+
+%% Part 3: Julia Sets
