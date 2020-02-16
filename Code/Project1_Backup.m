b9f60cb599c50999d413a7ea522d8ed25b48c425
Nikos Trembois <ntrembois@gmail.com>
2020-02-16 09:54:42 -0800

    Changed file names

--- Code/Project1_Backup.m
+++ Code/Project1_Backup.m
@@ -3,17 +3,16 @@
 % Nikos Trembois, Caitlin Brown, and Shuai Zhi
 
 global c xRange yRange pts bsave
-% Constants that will be used for z^2 - c plots
 c = [0.36 + 0.1i, -.123 - .745i,-.749, -1.25];
-xRange = [1, 1, 1, 1]; % Range of x values for plotting window
-yRange = [1, 1, 1, 1]; % Range of y values for plotting window
-pts = 100; % Number of points in x and y directions of plot
-bsave = 0; % boolean value (0,1) to save plots to file or not
+xRange = [1, 1, 1, 1];
+yRange = [1, 1, 1, 1];
+pts = 100;
+bsave = 0;
 
 %% Part 1: Fractals
-phi = @(z,c) z^2; % Defining the function
-% Iterating the Julia Set with function defined at bottom of script
-M = FilledJuliaSet(phi,1,1,pts,0,'nocolor',2);
+clearvars -except c xRange yRange pts bsave
+phi = @(z,c) z^2;
+M = FilledJuliaSet(phi,1,1,100,0);
 
 figure(); hold on
 title('Filled Julia Set of $\phi = z^2$','Fontsize',16,'Interpreter','Latex')
@@ -32,23 +31,22 @@ end
 %% Part 2:  Fractals
 clearvars -except c xRange yRange pts bsave
 phi = @(z,c) z^2 + c;
+xrange = 2; yrange = 2;
 M = cell(length(c),1);
 for i = 1:length(c)
-    M{i} = FilledJuliaSet(phi,xRange(1),yRange(1),100,c(i),'nocolor',2);
+    M{i} = FilledJuliaSet(phi,xrange,yrange,100,c(i));
 end
 
-% Plotting the maps of orbits
 for i = 1:length(c)
     figure(); hold on
-    % Generate the title string based on constant value
     if (real(c(i) > 0 ))
         stitle = strcat('Filled Julia Set of $z^2 + ',num2str(c(i)),'$');
     else
         stitle = strcat('Filled Julia Set of $z^2 ',num2str(c(i)),'$');
     end
-    title(stitle,'Interpreter','Latex','FontSize',24) % Create Title
-    colormap([1 0 0; 1 1 1]); % Define colors for map
-    image( [-xRange xRange], [-yRange yRange], M{i}') % create map
+    title(stitle,'Interpreter','Latex','FontSize',24)
+    colormap([1 0 0; 1 1 1]);
+    image( [-xrange xrange], [-yrange yrange], M{i}')
     axis xy
     axis equal
     ax = gca;
@@ -57,7 +55,7 @@ for i = 1:length(c)
     xlabel('\Re','Fontsize',18)
     ylabel('\Im','Fontsize',18)
     hold off
-    if bsave == 1 % Save plot to file if bsave = 1
+    if bsave == 1
         ssave = strcat('../Figures/FilledJulia',num2str(i),'.png');
         saveas(gcf,ssave)
     end
@@ -65,16 +63,15 @@ end
 
 %% Part 3: Julia Sets
 clearvars -except c xRange yRange pts bsave
-psi = @(z,c) sqrt(z - c); % Define plot 
-x = zeros(pts,length(c)); % initialize a x vector for each constant
-y = zeros(pts,length(c)); % initialize a y vector for each constant
+psi = @(z,c) sqrt(z - c);
+x = zeros(100,4);
+y = zeros(100,4);
 for k = 1:length(c)
     clear z;
     z = c(k);
-    for j = 1:1000 % Use 1,000 iterations
+    for j = 1:1000
         x(j,k) = real(z(j));
         y(j,k) = imag(z(j));
-        % Randomly choose positive or negative root with equal weighting
         if randi([0 1],1,1) == 1
             z(j+1) = psi(z(j),c(k));
         else
@@ -83,25 +80,22 @@ for k = 1:length(c)
     end
 end
 
-% Plot the Julia sets for different constant values
 for i = 1:length(c)
     figure(); hold on
-    % Generate the title string based on constant value
     if (real(c(i) > 0 ))
         stitle = strcat('Julia Set of $z^2 + ',num2str(c(i)),'$');
     else
         stitle = strcat('Julia Set of $z^2 ',num2str(c(i)),'$');
     end
-    title(stitle,'Interpreter','Latex','FontSize',24) % Create title
-    scatter(x(:,i),y(:,i),'filled') % Plot the Julia Set
-    % Plot Formatting
+    title(stitle,'Interpreter','Latex','FontSize',24) 
+    scatter(x(:,i),y(:,i),'filled')
     ax = gca;
     plot(ax.XLim,[0,0],'LineStyle','--','Color',[.5,.5,.5])
     plot([0,0],ax.YLim,'LineStyle','--','Color',[.5,.5,.5])
     xlabel('\Re','Fontsize',18)
     ylabel('\Im','Fontsize',18)
     hold off
-    if bsave == 1 % Save plot to file if bsave = 1
+    if bsave == 1
         ssave = strcat('../Figures/Julia',num2str(i),'.png');
         saveas(gcf,ssave)
     end
@@ -113,11 +107,11 @@ end
 
 %% Part 5: Connectivity of the Julia Set
 clearvars -except c xRange yRange pts bsave
-psi = @(z) z^2 + 3; % Calculate inverse iteration undder some function psi
-z = 0; % Initialize value
-max_iter = 1000; % Use 1000 iterations to determine if orbit diverges
+psi = @(z) z^2 + 3;
+z = 0;
+max_iter = 1000;
 for i = 1:max_iter
-    z = psi(z); % Calculate the orbit
+    z = psi(z);
     if abs(z) > 100
         fprintf('The orbit diverged after %i iterations, the set is not connected\n',i)
         break
@@ -132,58 +126,21 @@ end
 %% Part 6: Coloring Divergent Orbits
 clearvars -except c xRange yRange pts bsave
 phi = @(z,c) z^2 - c;
-for i = 1:length(c)
-    % Once again using the Filled Julia Set function with options 
-    % 'colored' to color plots based on iterations to converge to diverge
-    % past an absolute value of 100
-    M{i} = FilledJuliaSet(phi, xRange(i), yRange(i), pts, c(i), 'colored', 100);
-end
-
-% Plotting the Diverging orbits
-for i = 1:length(c)
-    figure(); hold on
-    % plot map of diverging orbits
-    image( [-xRange(i) xRange(i)], [-yRange(i) yRange(i)], M{i}')
-    % Plot formatting
-    axis xy
-    axis equal
-    ax = gca;
-    ax.XLim = [-xRange(i) xRange(i)]; ax.YLim = [-yRange(i) yRange(i)];
-    plot(ax.XLim,[0,0],'LineStyle','--','Color',[.5,.5,.5])
-    plot([0,0],ax.YLim,'LineStyle','--','Color',[.5,.5,.5])
-    xlabel('\Re','Fontsize',18)
-    ylabel('\Im','Fontsize',18)
-    colormap(jet(max(max(M{i})))) % Set Color map range to 100
-    colorbar
-    hold off
-    if bsave == 1 % Save plot to file if bsave = 1
-        ssave = strcat('../Figures/ColoredJulia',num2str(i),'.png');
-        saveas(gcf,ssave)
-    end
-end
-
-%% Part 7: Newton's Method in the Complex Plane
-clearvars -except c xRange yRange pts bsave
-% Defining function that represents f/f' for a general f = z^n - 1
-g = @(z,n) (z^n - 1)/(n*z^(n-1));
-a = linspace(-5,5,500); % set plottig window
-b = linspace(-5,5,500);
-nmax = 5; % The maximum polynomial order to plot to
-M = cell(nmax,1);
-for n = 2:nmax % start with order 2 as order 1 is not interesting
-    M{n-1} = 100*ones(length(a),length(b)); % initialize values to
-    gn = @(z) (z^n - 1)/(n*z^(n-1)); % Redefine g for specific value of n
+rl = -1; ru = -rl; %1.6
+il = -1;  iu = -il; %.7
+a = linspace(rl,ru,100);
+b = linspace(il,iu,100);
+M = cell(length(c),1);
+for k = 1:length(c)
+    M{k} = ones(length(a),length(b));
     for r = 1:length(a)
         for i = 1:length(b)
+            clear z;
             z = a(r) + 1i*b(i);
             for j = 1:100
-                if abs(z^n-1) > 0.001 % Testing for convergence
-                    z = z - gn(z); % Newton's Iteration
-                else
-                    if j < 3 % Printing values of possible roots
-                        fprintf('For n = %i, The root is near %2.4f, %2.4f\n',n,a(r),b(i));
-                    end
-                    M{n-1}(r,i) = j; % Set value to number of iterations
+                z(j+1) = phi(z(j),c(k));
+                if abs(z(j+1)) > 100
+                    M{k}(r,i) = j;
                     break;
                 end
             end
@@ -191,26 +148,71 @@ for n = 2:nmax % start with order 2 as order 1 is not interesting
     end
 end
 
-% Plotting the Newton iterations
+for i = 1:length(c)
+    figure(); hold on
+    image( [rl ru], [il iu], M{i}')
+    axis xy
+    axis equal
+    ax = gca;
+    ax.XLim = [rl,-rl]; ax.YLim = [il,-il];
+    plot(ax.XLim,[0,0],'LineStyle','--','Color',[.5,.5,.5])
+    plot([0,0],ax.YLim,'LineStyle','--','Color',[.5,.5,.5])
+    xlabel('\Re','Fontsize',18)
+    ylabel('\Im','Fontsize',18)
+    colormap(jet(max(max(M{i}))))
+    colorbar
+    hold off
+    if bsave == 1
+        ssave = strcat('../Figures/ColoredJulia',num2str(i),'.png');
+        saveas(gcf,ssave)
+    end
+end
+
+%% Part 7: Newton's Method in the Complex Plane
+clearvars -except c xRange yRange pts bsave
+g = @(z,n) (z^n - 1)/(n*z^(n-1));
+a = linspace(-5,5,500);
+b = linspace(-5,5,500);
+nmax = 5;
+M = cell(nmax,1);
+for n = 2:nmax
+    M{n-1} = 100*ones(length(a),length(b));
+    gn = @(z) (z^n - 1)/(n*z^(n-1));
+    for r = 1:length(a)
+        for i = 1:length(b)
+            z = a(r) + 1i*b(i);
+            for j = 1:100
+                diff = z^n - 1;
+                if abs(z^n-1) > 0.001
+                    z = z - gn(z);
+                else
+                    if j < 3
+                        fprintf('For n = %i, The root is near %2.4f, %2.4f\n',n,a(r),b(i));
+                    end
+                    M{n-1}(r,i) = j;
+                    break;
+                end
+            end
+        end
+    end
+end
+
 stitle1 = 'Fixed points of';
 for i = 1:nmax-1
     figure(); hold on
-    % Creating title string
     stitle2 = strcat( ' $z^',num2str(i+1),' - 1$');
-    title(strcat(stitle1,stitle2),'Interpreter','Latex')
-    % Plotting map of iterations of convergence
     image( [min(a) max(a)], [min(b) max(b)], M{i}')
-    % Plot formatting
     colormap(jet(max(max(M{i}))))
     colorbar
     axis xy
     ax = gca;
     axis equal
     ax.XLim = [min(a) max(a)]; ax.YLim = [min(b) max(b)];
+    title(strcat(stitle1,stitle2),'Interpreter','Latex')
     xlabel('\Re','Fontsize',18)
     ylabel('\Im','Fontsize',18)
     hold off
-    if bsave == 1 % Save plot to file if bsave = 1
+    if bsave == 1
         ssave = strcat('../Figures/Newton',num2str(i),'.png');
         saveas(gcf,ssave)
     end
@@ -219,18 +221,18 @@ end
 
 %% Part 8: Mandelbrot Set
 clearvars -except c xRange yRange pts bsave
-phi = @(z,c) z^2 + c; % function to plot
-a = linspace(-1,1,500); % Plotting Window
+phi = @(z,c) z^2 + c;
+a = linspace(-1,1,500);
 b = linspace(-1,1,500);
 M = ones(length(a),length(b));
 
 for r = 1:length(a)
     for i = 1:length(b)
         clear z;
-        z = 0; % start with z = 0
-        cm = a(r) + b(i)*1i;
+        z = 0;
+        c = a(r) + b(i)*1i;
         for j = 1:100
-            z(j+1) = phi(z(j),cm);
+            z(j+1) = phi(z(j),c);
             if abs(z(j+1)) > 100
                 M(r,i) = j;
                 break;
@@ -239,7 +241,6 @@ for r = 1:length(a)
     end
 end
 
-% Plot Mandelbrot set
 figure(); hold on
 title('Mandelbrot Set')
 xlabel('Real')
@@ -250,7 +251,7 @@ colorbar
 axis xy
 axis('equal')
 axis([ -1 1 -1 1])
-if bsave == 1 % Save plot to file if bsave = 1
+if bsave == 1
     saveas(gcf,'../Figures/Madelbrot.png')
 end
 %% Part 8 (cont) Zoom in
@@ -265,9 +266,9 @@ for r = 1:length(a)
     for i = 1:length(b)
         clear z;
         z = 0;
-        cm = a(r) + b(i)*1i;
+        c = a(r) + b(i)*1i;
         for j = 1:100
-            z(j+1) = phi(z(j),cm);
+            z(j+1) = phi(z(j),c);
             if abs(z(j+1)) > 100
                 M(r,i) = j;
                 break;
@@ -321,7 +322,29 @@ function FractalDimension(M,r)
 end
 
 % This function determines if a point is in a Julia Set
-function [ M ] = FilledJuliaSet(phi, xrange, yrange, pts, c, colored, maxvalue)
+function [ M ] = FilledJuliaSet(phi, xrange, yrange, pts, c)
+    a = linspace(-xrange, xrange, pts);
+    b = linspace(-yrange, yrange, pts);
+    for k = 1:length(c)
+        M = ones(length(a), length(b));
+        for r = 1:length(a)
+            for i = 1:length(b)
+                clear z;
+                z = a(r) + 1i*b(i);
+                for j = 1:100
+                    z(j+1) = phi( z(j), c(k) );
+                    if abs( z(j+1) ) > 2
+                        M(r,i) = 2;
+                        break;
+                    end
+                end
+            end
+        end
+    end
+end
+
+% This function determines if a point is in a Julia Set
+function [ M ] = FilledJuliaSet2(phi, xrange, yrange, pts, c, colored, maxvalue)
     a = linspace(-xrange, xrange, pts);
     b = linspace(-yrange, yrange, pts);
     for k = 1:length(c)
@@ -333,12 +356,15 @@ function [ M ] = FilledJuliaSet(phi, xrange, yrange, pts, c, colored, maxvalue)
                 for j = 1:100
                     z(j+1) = phi( z(j), c(k) );
                     if abs( z(j+1) ) > maxvalue
-                        if (colored == 'colored')
-                            % If the colored option is 'colored', then
-                            % color the julia set by the number of 
-                            % iterations to converge
+                        if (colored == 1)
+                            % If the colored option is true, color the
+                            % julia set by the number of iterations to
+                            % converge
                             M(r,i) = j;
-                        else % otherwise give discrete value
+                        else
+                            % if colored option is used, then give a single
+                            % value based on convergence, independent of
+                            % the number of iterations required to converge
                             M(r,i) = 2;
                         end
                         break
